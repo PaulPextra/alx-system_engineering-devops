4387642663f5896834d02acaeff5bdf07c5318f4
Paul Okoli <paulokoli7@gmail.com>
2023-02-28 10:33:57 +0000

    Adding 2-recurse.py file

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from json import loads
+from requests import get
+
+def recurse(subreddit, hot_list=[]):
+    """recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return None.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 \
+	(KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36'
+    }
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
+
+    try:
+        children = reddits.get('data').get('children')
+        for title in children:
+            hot_list.append(title.get('data').get('title'))
+        return hot_list
+    except:
+        print(None)
+        return 0
